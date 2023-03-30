0308129d5d5a194570308bbb1fe64f514af32425
Yaroslav <sych521@gmail.com>
2023-03-30 19:29:35 +0300

    first draft

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.getElementsByClassName("gallery")[0];
 
-console.log(galleryItems);
+function doMarkup(objectOfImages) {
+  const ss = objectOfImages
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+        `;
+    })
+    .join("");
+  return ss;
+}
+
+galleryList.insertAdjacentHTML("beforeend", doMarkup(galleryItems));
+
+galleryList.addEventListener("click", clickFunc);
+function clickFunc(event) {
+  event.preventDefault();
+  const element = event.target;
+  document.addEventListener("keydown", closeOnEscape);
+
+  if (element.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${element.dataset.source}" alt="${element.alt}">`,
+    {
+      onClose: () => {
+        document.removeEventListener("keydown", closeOnEscape);
+      },
+    }
+  );
+  instance.show();
+
+  function closeOnEscape(event) {
+    event.nodeName === "ESCAPE";
+    instance.close();
+  }
+}
